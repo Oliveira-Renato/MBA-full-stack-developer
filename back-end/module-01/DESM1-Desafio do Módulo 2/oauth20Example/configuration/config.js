a0f749f4f57681618773cf3ea5840aeb1bfa41db
Renato <renato.printf@gmail.com>
2023-04-15 23:16:27 -0300

    googleauth + passport done

---
+++ back-end/module-01/DESM1-Desafio do Módulo 2/oauth20Example/configuration/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "client_id": "19265282652-njntvolipeulj0tp3ddu4j9cnkn65i3r.apps.googleusercontent.com",
+    "client_secret": "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",
+    "callback_url": "http://localhost:3000/auth/google/callback"
+}
