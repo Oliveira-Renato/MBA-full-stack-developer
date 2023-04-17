e4de76cd37851fea941585300c1dd263bac33f97
Renato <renato.printf@gmail.com>
2023-04-17 01:33:03 -0300

    modulo-3 added

--- back-end/module-01/DESM1-Desafio do Módulo 2/oauth20Example/configuration/config.js
+++ back-end/module-01/DESM1-Desafio do Módulo 2/oauth20Example/configuration/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "facebook_app_id": "app_id",
-    "facebook_app_secret": "app_secret",
+    "facebook_app_id": "795119524883589",
+    "facebook_app_secret": "8b9c2f158fe45987sadasdsa",
     "facebook_callback_url": "http://localhost:3000/auth/facebook/callback"
 }
