f07dbbaa18a8020334f50ecd799eefc83608dbfc
Renato <renato.printf@gmail.com>
2023-05-14 17:48:15 -0300

    autor services done

--- back-end/desafio-final/bookstore_api/services/autores.service.js
+++ back-end/desafio-final/bookstore_api/services/autores.service.js
@@ -1,13 +1,29 @@
-import express from 'express'
-import autoresController from '../controllers/autores.controller.js'
+import autoresRepository from "../repositories/autores.repository.js"
 
-const router = express.Router()
+async function getAutores(req, res, next) {
+  return await autoresRepository.getAutores();
+}
 
-router.get('/', (req, res) => res.send('Bookstore API'))
-router.get('/autores', autoresController.getAutores)
-router.get('/autores/:id', autoresController.getAutor)
-router.post('/autores', autoresController.createAutor)
-router.put('/autores', autoresController.updateAutor)
-router.delete('/autores/:id', autoresController.deleteAutor)
+async function getAutor(pAutorId) {
+  return await autoresRepository.getAutor(pAutorId);
+}
 
-export default router
+async function createAutor(pAutor) {
+  return await autoresRepository.createAutor(pAutor);
+}
+
+async function updateAutor(pAutor) {
+  return await autoresRepository.updateAutor(pAutor);
+}
+
+async function deleteAutor(pAutor) {
+  return await autoresRepository.deleteAutor(pAutor);
+}
+
+export default {
+  createAutor,
+  updateAutor,
+  deleteAutor,
+  getAutores,
+  getAutor
+}
