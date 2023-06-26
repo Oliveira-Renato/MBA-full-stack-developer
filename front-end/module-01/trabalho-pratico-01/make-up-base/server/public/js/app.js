c0b19830d93231845d953ab3affd3199787d11e4
Renato <renato.printf@gmail.com>
2023-06-25 21:41:32 -0300

    tarefa finalizada

--- front-end/module-01/trabalho-pratico-01/make-up-base/server/public/js/app.js
+++ front-end/module-01/trabalho-pratico-01/make-up-base/server/public/js/app.js
@@ -4,7 +4,8 @@ async function makeupProducts() {
     const req = await fetch('http://makeup-api.herokuapp.com/api/v1/products.json')
     const products = await req.json()
 
-    return products
+    //retorna uma lista de produtos ordenadas pela avaliação
+    return products.sort((a, b) => b.rating - a.rating);
   } catch (error) {
     console.log(error)
   }
@@ -16,23 +17,35 @@ async function productItem(pProduct) {
   const catalog = document.querySelector('.catalog') 
   let item = '<div><h2>Nenhum produto disponivel!</h2></div>'
 
+  function convertPrice(value) {
+    const convertedValue = value * 5.5;
+    const formattedValue = convertedValue.toFixed(2);
+    return formattedValue;
+  }
+
   if(products.length > 0) {
     item = ''//description
+
     for(elem in products) {
-      item += `<div class="product" data-name="${products[elem]['name']}" data-brand="${products[elem]['brand']}" data-type="${products[elem]['product_type']}" tabindex="${products[elem]['id']}">
-  <figure class="product-figure">
-    <img src="${products[elem]['image_link']}" width="215" height="215" alt="${products[elem]['name']}" onerror="javascript:this.src='img/unavailable.png'">
-  </figure>
-  <section class="product-description">
-    <h1 class="product-name">${products[elem]['name']}</h1>
-    <div class="product-brands"><span class="product-brand background-brand">${products[elem]['brand']}</span>
-<span class="product-brand background-price">R$ ${products[elem]['price']}</span></div>
+      let price = products[elem]['price'] === null ? 0 : convertPrice(products[elem]['price'])
+      let category = products[elem]['category'] === null ? '' : products[elem]['category']
+      
+      item += `<div class="product" onclick="loadDetails(event)" data-name="${products[elem]['name']}" data-brand="${products[elem]['brand']}" data-type="${products[elem]['product_type']}" tabindex="${products[elem]['id']}" data-category="${category}" data-rating="${products[elem]['rating']}" data-price="${price}" ">
+      <figure class="product-figure">
+        <img src="${products[elem]['image_link']}" width="215" height="215" alt="${products[elem]['name']}" onerror="javascript:this.src='img/unavailable.png'">
+      </figure>
+      <section class="product-description">
+        <h1 class="product-name">${products[elem]['name']}</h1>
+        <div class="product-brands"><span class="product-brand background-brand">${products[elem]['brand']}</span>
+    <span class="product-brand background-price">R$ ${price}</span></div>
   </section>
+  <div class="description" id="${products[elem]['id']}"></div>
 </div>`;
     }
   }
   catalog.innerHTML = item
 }
+
 //Pega as marcas e retorna no select
 async function getBrands() {
   const products = await makeupProducts();
@@ -109,40 +122,57 @@ async function getProductByName() {
   })
 }
 
-(async () => {
-  await productItem()
-  await getBrands()
-  await getCategories()
-  await getProductByName()
-})()
 //EXEMPLO DO CÓDIGO PARA OS DETALHES DE UM PRODUTO
-function loadDetails(product) {
+function loadDetails(event) {
+  const productElement = event.currentTarget;
+  const index = productElement.getAttribute('tabindex');
+  const price = productElement.getAttribute('data-price');
+  const brand = productElement.getAttribute('data-brand');
+  const type = productElement.getAttribute('data-type');
+  const category = productElement.getAttribute('data-category');
+  const rating = productElement.getAttribute('data-rating');
+  const product = { price, brand, type, category, rating };
+
+  let description = document.getElementById(index);
   let details = `<section class="product-details"><div class="details-row">
         <div>Brand</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">nyx</div>
+          <div class="details-bar-bg" style="width= 250">${product.brand}</div>
         </div>
       </div><div class="details-row">
         <div>Price</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">10.49</div>
+          <div class="details-bar-bg" style="width= 250">${product.price}</div>
         </div>
       </div><div class="details-row">
         <div>Rating</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">5</div>
+          <div class="details-bar-bg" style="width= 250">${product.rating}</div>
         </div>
       </div><div class="details-row">
         <div>Category</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250"></div>
+          <div class="details-bar-bg" style="width= 250">${product.category}</div>
         </div>
       </div><div class="details-row">
         <div>Product_type</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">bronzer</div>
+          <div class="details-bar-bg" style="width= 250">${product.type}</div>
         </div>
       </div></section>`;
+
+      description.innerHTML = details;
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await productItem()
+  await getBrands()
+  await getCategories()
+  await getProductByName()
+
+  const productDetail = document.querySelector('')
+});
+
+
 
 
