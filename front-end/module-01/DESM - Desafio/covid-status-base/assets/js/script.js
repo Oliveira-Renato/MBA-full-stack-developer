acc6d75caa953c6c5bad4fb38bcf0fcf49204f46
Renato <renato.printf@gmail.com>
2023-07-05 23:45:15 -0300

    GRAPHY bars done

--- front-end/module-01/DESM - Desafio/covid-status-base/assets/js/script.js
+++ front-end/module-01/DESM - Desafio/covid-status-base/assets/js/script.js
@@ -7,46 +7,35 @@ async function getCovidData() {
     console.error(error);
   }
 }
-//BASIC COVID INFO
-async function handleBasicCovidInfo() {
+async function getCovidDataByCountries() {
   try {
-    const CovidData = await getCovidData() || {}
-
-    return { 
-      cases :handleDecimals(CovidData.cases),
-      deaths : handleDecimals(CovidData.deaths),
-      recovered : handleDecimals(CovidData.recovered)
-    }
+    const response = await axios.get('https://disease.sh/v3/covid-19/countries');
+    const data =  await response.data;
+    // Ordenar o array de objetos com base no número de mortes em ordem decrescente e retorna o mesmo
+    return data.sort((a, b) => b.deaths - a.deaths);
   } catch (error) {
-    console.log(error)
+    console.error(error);
   }
 }
+
 //OUTPUT THE KPI DATA IN THE KPI SECTION INFO
 async function handleKPIinfo() {
   try {
-    const CovidData = await handleBasicCovidInfo() || {}
+    const CovidData = await getCovidData() || {}
     let vCases = document.getElementById('confirmed')
     let vDeaths = document.getElementById('death')
     let vRecovered = document.getElementById('recovered')
 
     if(Object.keys(CovidData).length > 0) {
-      vCases.innerHTML = CovidData.cases
-      vDeaths.innerHTML = CovidData.deaths
-      vRecovered.innerHTML = CovidData.recovered
+      vCases.innerHTML = handleDecimals(CovidData.cases)
+      vDeaths.innerHTML = handleDecimals(CovidData.deaths)
+      vRecovered.innerHTML = handleDecimals(CovidData.recovered)
     } else {
       throw new Error('No covid data')
     }
   } catch (error) {
     console.log(error)
   }
-}
-//FORMAT NUMBER TO DECIMALS
-function handleDecimals(pNumber) {
-  let vNumber = pNumber;
-  let vOptions = { minimumFractionDigits: 0, maximumFractionDigits: 0};
-  let vFormattedNumber = vNumber.toLocaleString(undefined, vOptions);
-
-  return vFormattedNumber.replaceAll(',','.');
 }
 //PIZZA GRAPHY
 async function handlePyzzaGraphyInfo() {
@@ -85,8 +74,71 @@ async function handlePyzzaGraphyInfo() {
     options: options
   });
 }
+//BAR GRAPHY
+async function handleBarGraphyInfo() {
+  const covidData = await getCovidDataByCountries() || {}
+  const barCvx = document.getElementById('barras');
+
+  let deaths = covidData.slice(0,10).map(({deaths}) => deaths)
+  let countries = covidData.slice(0,10).map(({country}) => country)
+
+  const data = {
+    labels: countries,
+    datasets: [{
+      label: 'Total de mortes por país - Top 10',
+      data: deaths,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(55, 200, 70, 0.2)',
+        'rgba(21, 100, 207, 0.2)',
+        'rgba(201, 80, 207, 0.2)',
+        'rgba(111, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(134, 203, 207)',
+        'rgb(452, 203, 207)',
+        'rgb(255, 100, 207)',
+        'rgb(111, 120, 207)'
+      ],
+      borderWidth: 1
+    }],
+  };
+  
+  new Chart(barCvx, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  });
+}
+
+//FORMAT NUMBER TO DECIMALS
+function handleDecimals(pNumber) {
+  let vNumber = pNumber;
+  let vOptions = { minimumFractionDigits: 0, maximumFractionDigits: 0};
+  let vFormattedNumber = vNumber.toLocaleString(undefined, vOptions);
+
+  return vFormattedNumber.replaceAll(',','.');
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
   await handleKPIinfo()
   await handlePyzzaGraphyInfo()
+  await handleBarGraphyInfo()
 });
