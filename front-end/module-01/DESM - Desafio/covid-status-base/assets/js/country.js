c130c37faeab2e8cc664738e7cdde4b00ba0f42b
Renato <renato.printf@gmail.com>
2023-07-08 23:38:23 -0300

    LINE graphy done

--- front-end/module-01/DESM - Desafio/covid-status-base/assets/js/country.js
+++ front-end/module-01/DESM - Desafio/covid-status-base/assets/js/country.js
@@ -37,6 +37,82 @@ async function handleKPIinfoByCountry(pCountry) {
     console.log(error)
   }
 }
+
+
+async function filter(pOption = 1, pCountry, pInicialDate, pFinalDate) {
+  const covidData = await getCovidHistoricalByCountry(pCountry) || {}
+  const { country } = covidData
+
+  console.log(covidData)
+  const { deaths, cases, recovered } = covidData.timeline
+  let resultData = []
+
+  const getInfo = (pFilter) => {
+    Object.keys(pFilter).forEach(elem => {
+      resultData.push({
+        country,
+        numbers : pFilter[elem],
+        date: handleDateFormat(elem),
+      })
+    })
+  }
+
+  switch(pOption) {
+    case 2://cases
+      getInfo(cases)
+      break;
+    case 3://recovered
+      getInfo(recovered)
+      break;
+    default://deaths
+      getInfo(deaths)
+      break;
+  }
+
+  console.log(resultData)
+  handleLineGraphyInfo()
+}
+
+
+
+//PIZZA GRAPHY
+function handleLineGraphyInfo(pLabels = [], pData = []) {
+  const lineCvx = document.getElementById('linhas');
+
+  let dados = {
+    labels: pLabels,
+    datasets: [{
+      label: 'Número de Mortes',
+      data: pData,
+      borderColor: 'rgb(255, 205, 86)',
+      backgroundColor: 'rgb(255, 155, 80)',
+      fill: false,
+      hoverOffset: 4
+    }]
+  }
+  
+  let options =  {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Curva diária de Covid-19'
+      },
+    },
+    interaction: {
+      intersect: false,
+    }
+  }
+  
+  new Chart(lineCvx, {
+    type: 'line',
+    data: dados,
+    options: options
+  });
+}
 //FORMAT NUMBER TO DECIMALS
 function handleDecimals(pNumber) {
   let vNumber = pNumber;
@@ -45,7 +121,40 @@ function handleDecimals(pNumber) {
 
   return vFormattedNumber.replaceAll(',','.');
 }
+//FORMAT DATE
+function handleDateFormat(pDate) {
+  let partes = pDate.split('/');
+  let mes = partes[0];
+  let dia = partes[1];
+  let ano = '20' + partes[2];
+
+  const adicionarZeroEsquerda =  (numero) => {
+    return numero < 10 ? '0' + numero : numero;
+  }
+
+  let dataObj = new Date(ano, mes - 1, dia);
+  let diaFormatado = adicionarZeroEsquerda(dataObj.getDate());
+  let mesFormatado = adicionarZeroEsquerda(dataObj.getMonth() + 1);
+  let anoFormatado = dataObj.getFullYear();
+  
+  return diaFormatado + '/' + mesFormatado + '/' + anoFormatado;
+}
+//FILTER DATE AND RETURN AN ARRAY OF IT
+function verificarData(dataReferencia, dataInicio, dataFim) {
+  // Converter as datas para objetos do tipo Date
+  const dataRef = new Date(dataReferencia);
+  const inicio = new Date(dataInicio);
+  const fim = new Date(dataFim);
+
+  // Verificar se a data de referência está entre as datas de início e fim
+  if (dataRef >= inicio && dataRef <= fim) {
+    return true; // A data está entre as datas especificadas
+  } else {
+    return false; // A data está fora do intervalo especificado
+  }
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
   await handleKPIinfoByCountry()
+  handleLineGraphyInfo()
 });
