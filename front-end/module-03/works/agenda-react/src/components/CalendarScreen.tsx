b1ada512264a7919e4a46aeb03d304170a4bc234
Renato <renato.printf@gmail.com>
2023-07-25 00:40:07 -0300

    button and checkbox

--- front-end/module-03/works/agenda-react/src/components/CalendarScreen.tsx
+++ front-end/module-03/works/agenda-react/src/components/CalendarScreen.tsx
@@ -1,3 +1,4 @@
+import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -5,45 +6,75 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
+import Button from '@mui/material/Button';
+
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 const DAYS_OF_WEEK = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB']
 
 export function CalendarScreen() {
 
   return (
-    <TableContainer sx={{ height: "100%" }} component={"div"}>
-      <Table sx={{
-        minHeight: "100%",
-        "& td ~ td, th ~ th": {
-          border: "1px solid rgb(224, 224, 224)"
-        }
-      }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {DAYS_OF_WEEK.map((day) => (
-              <TableCell align="center" key={day}>{day}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
+    <Box sx={{
+      display: "flex",
+      height: "100%",
+      alignItens: "stretch"
+    }}>
+      {/* left */}
+      <Box sx={{
+        borderRight: "1px solid rgb(244, 244, 244)",
+        padding: "8px 16px",
+        width: "14em"
+      }}>
+        <h2>Agenda React</h2>
+        <Button variant="contained">Nova agenda</Button>
 
-          <TableRow>
-            {DAYS_OF_WEEK.map((day) => (
-              <TableCell align="center" key={day}>x</TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {DAYS_OF_WEEK.map((day) => (
-              <TableCell align="center" key={day}>x</TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {DAYS_OF_WEEK.map((day) => (
-              <TableCell align="center" key={day}>x</TableCell>
-            ))}
-          </TableRow>
+        <Box sx={{ marginTop: "64px" }}>
+          <h3>Agendas</h3>
+          <FormGroup>
+            <FormControlLabel control={<Checkbox defaultChecked />} label="Pessoal" />
+            <FormControlLabel control={<Checkbox />} label="Trabalho" />
+          </FormGroup>
+        </Box>
+      </Box>
 
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <TableContainer component={"div"}>
+        <Table sx={{
+          minHeight: "100%",
+          "& td ~ td, th ~ th": {
+            border: "1px solid rgb(224, 224, 224)"
+          }
+        }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {DAYS_OF_WEEK.map((day) => (
+                <TableCell align="center" key={day}>{day}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+            <TableRow>
+              {DAYS_OF_WEEK.map((day) => (
+                <TableCell align="center" key={day}>x</TableCell>
+              ))}
+            </TableRow>
+            <TableRow>
+              {DAYS_OF_WEEK.map((day) => (
+                <TableCell align="center" key={day}>x</TableCell>
+              ))}
+            </TableRow>
+            <TableRow>
+              {DAYS_OF_WEEK.map((day) => (
+                <TableCell align="center" key={day}>x</TableCell>
+              ))}
+            </TableRow>
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
